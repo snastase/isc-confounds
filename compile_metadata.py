d5421248dfd825d18636b09288ae9d5c68373a84
Sam Nastase <sam.nastase@gmail.com>
2020-05-26 10:39:32 -0400

    Minor modification to metadata

--- compile_metadata.py
+++ compile_metadata.py
@@ -5,13 +5,14 @@ from natsort import natsorted
 
 
 base_dir = '/jukebox/hasson/snastase/isc-confounds'
-data_dir = '/jukebox/hasson/snastase/narratives/derivatives/fmriprep'
+preproc_dir = '/jukebox/hasson/snastase/narratives/derivatives/fmriprep'
+afni_dir = '/jukebox/hasson/snastase/narratives/derivatives/afni'
 
 
 # Get our subject list from the BIDS directory
 n_subjects = 345
 subjects = natsorted([p.split('/')[-2] for p in
-                      glob(join(data_dir, 'sub-*/'))])
+                      glob(join(preproc_dir, 'sub-*/'))])
 
 # Check that we're not missing subjects
 for n, subject in zip(range(1, n_subjects + 1), subjects):
@@ -21,13 +22,14 @@ for n, subject in zip(range(1, n_subjects + 1), subjects):
 
 # Create a dictionary containing filenames keyed to subjects
 spaces = ['MNI152NLin2009cAsym', 'fsaverage6']
+width = 'sm6'
 subject_meta = {}
 for subject in subjects:
     subject_meta[subject] = {'bold': {},
                              'confounds': []}
 
     # Grab confounds TSV file
-    confound_fns = glob(join(data_dir, subject, 'func',
+    confound_fns = glob(join(preproc_dir, subject, 'func',
                              f'{subject}*desc-confounds'
                              '_regressors.tsv'))
     subject_meta[subject]['confounds'] = confound_fns
@@ -35,16 +37,26 @@ for subject in subjects:
     # Grab either volumetric or surface-based BOLD filenames
     for space in spaces:
         if space[:2] == 'fs':
-            bold_fns = glob(join(data_dir, subject, 'func',
-                                 f'{subject}*space-{space}*func.gii'))
-            assert len(bold_fns) == 2 * len(confound_fns)
-        else:
-            bold_fns = glob(join(data_dir, subject, 'func',
-                                 f'{subject}*space-{space}*desc-'
-                                 'preproc_bold.nii.gz'))
-            assert len(bold_fns) == len(confound_fns)
+            preproc_fns = glob(join(preproc_dir, subject, 'func',
+                                    f'{subject}*space-{space}*func.gii'))
+            assert len(preproc_fns) == 2 * len(confound_fns)
+            
+            smooth_fns = glob(join(afni_dir, subject, 'func',
+                                   (f'{subject}*space-{space}*'
+                                    f'desc-{width}*func.gii')))
 
-        subject_meta[subject]['bold'][space] = bold_fns
+        else:
+            preproc_fns = glob(join(preproc_dir, subject, 'func',
+                                    f'{subject}*space-{space}*desc-'
+                                 'preproc_bold.nii.gz'))
+            assert len(preproc_fns) == len(confound_fns)
+
+            smooth_fns = glob(join(afni_dir, subject, 'func',
+                                   (f'{subject}*space-{space}*'
+                                    f'desc-{width}*_bold.nii.gz')))
+
+        subject_meta[subject]['bold'][space] = {'preproc': preproc_fns,
+                                                width: smooth_fns}
 
 # Save the subject metadata dictionary
 with open(join(base_dir, 'subject_meta.json'), 'w') as f:
@@ -63,6 +75,7 @@ tasks = natsorted(tasks)
 
 # Create a dictionary of filenames keyed to tasks for convenience
 task_meta = {}
+descs = ['preproc', 'sm6']
 for task in tasks:
     task_meta[task] = {}
     for subject in subject_meta:
@@ -82,11 +95,13 @@ for task in tasks:
                     task_meta[task][subject]['confounds'].append(
                         confound_fn)
             for space in subject_meta[subject]['bold']:
-                task_meta[task][subject]['bold'][space] = []
-                for bold_fn in subject_meta[subject]['bold'][space]:
-                    if f"task-{task}_" in bold_fn:
-                        task_meta[task][subject]['bold'][space].append(
-                            bold_fn)
+                task_meta[task][subject]['bold'][space] = {}
+                for desc in descs:
+                    task_meta[task][subject]['bold'][space][desc] = []
+                    for bold_fn in subject_meta[subject]['bold'][space][desc]:
+                        if f"task-{task}_" in bold_fn:
+                            task_meta[task][subject]['bold'][space][
+                                desc].append(bold_fn)
 
 # Save the task metadata dictionary
 with open(join(base_dir, 'task_meta.json'), 'w') as f:
